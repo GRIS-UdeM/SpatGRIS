45f576bd8f6f88c9c0e6d0852b00179a9362b6ec
Vincent Berthiaume <vberthiaume@gmail.com>
2016-12-02 11:25:31 -0500

    re #1: seems like deactivating the juce audio fixes this... We don't need it for now, but worth chekcing out later

--- Source/MainComponent.cpp
+++ Source/MainComponent.cpp
@@ -36,7 +36,7 @@ public:
         setSize (800, 600);
 
         // specify the number of input and output channels that we want to open
-        setAudioChannels (2, 2);
+//        setAudioChannels (2, 2);
     }
 
     ~MainContentComponent()
