cfda34f4f738c48d3fc5524136cffe1ae91280e8
Olivier Belanger <belangeo@gmail.com>
2017-10-05 21:30:40 -0400

    Upgraded version number to 0.0.8.

--- JuceLibraryCode/JuceHeader.h
+++ JuceLibraryCode/JuceHeader.h
@@ -43,8 +43,8 @@
 namespace ProjectInfo
 {
     const char* const  projectName    = "ServerGRIS";
-    const char* const  versionString  = "0.0.7";
-    const int          versionNumber  = 0x7;
+    const char* const  versionString  = "0.0.8";
+    const int          versionNumber  = 0x8;
 }
 #endif
 
