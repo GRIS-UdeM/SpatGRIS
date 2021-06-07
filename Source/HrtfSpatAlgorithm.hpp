6c42312e026ee14c52b9f1ec1c87a2cdb7b63236
Samuel Béland <samuel.beland@gmail.com>
2021-06-07 12:57:13 -0400

    Bumped the version number.
    Fixed the max input/output limit in the README.

--- Source/HrtfSpatAlgorithm.hpp
+++ Source/HrtfSpatAlgorithm.hpp
@@ -1,7 +1,7 @@
 /*
  This file is part of SpatGRIS.
 
- Developers: Samuel Béland, Olivier Bélanger, Nicolas Masson
+ Developers: Samuel BÃ©land, Olivier BÃ©langer, Nicolas Masson
 
  SpatGRIS is free software: you can redistribute it and/or modify
  it under the terms of the GNU General Public License as published by
