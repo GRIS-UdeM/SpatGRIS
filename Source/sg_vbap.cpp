3b0add0bd3beda5116e99aa7edfb485b78d0c310
OKGougou <glanelepine@gmail.com>
2022-04-06 17:17:01 -0400

    Added actual programmer's name in source files

--- Source/sg_vbap.cpp
+++ Source/sg_vbap.cpp
@@ -8,7 +8,7 @@
 /*
  This file is part of SpatGRIS.
 
- Developers: Samuel Béland, Olivier Bélanger, Nicolas Masson
+ Developers: Gaël Lane Lépine, Samuel Béland, Olivier Bélanger, Nicolas Masson
 
  SpatGRIS is free software: you can redistribute it and/or modify
  it under the terms of the GNU General Public License as published by
